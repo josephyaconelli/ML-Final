de6772457e0f2d9c544ef1adcae4badf8c8385c5
Liz Olson <lizolson95@gmail.com>
2017-03-11 20:09:45 -0800

    reads in dictionaries

--- proj.py
+++ proj.py
@@ -4,7 +4,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
+paramdict = {}
+factdict = {}
 
+
+def read_facts(filename):
+  f = open(filename, 'r')
+  p = re.compile(',')
+  f.readline()
+  f.readline()
+  count = 0
+  for l in f:
+    value = p.split(l.strip())
+    key = value.pop(0)
+    #print key
+    #print len(value[0])
+    if len(value[1]) < 1:
+      count += 1
+      continue
+    value.pop(0)
+    value.pop(0)
+    factdict[key] = [float(x) for x in value]
+    
+def read_param(filename):
+  f = open(filename, 'r')
+  p = re.compile(',')
+  f.readline()
+  for l in f: 
+    list = p.split(l.strip())
+    paramdict[list[0]] = list[1]
 
 def read_data(filename):
   f = open(filename, 'r')
@@ -33,16 +61,18 @@ def read_data(filename):
 def main(argv):
   # Process command line arguments.
   # (You shouldn't need to change this.)
-  if (len(argv) != 3):
-    print 'Usage: perceptron.py <train> <test> <model>'
+  if (len(argv) != 5):
+    print 'Usage: perceptron.py <train> <test> <facts> <dictionary> <model>'
     sys.exit(2)
-  (trainx, trainy, varnames) = read_data(argv[0])
-  (testx, testy, testvarnames) = read_data(argv[1])
+  read_param(argv[3])
+  #(trainx, trainy, varnames) = read_data(argv[0])
+  #(testx, testy, testvarnames) = read_data(argv[1])
   #modelfile = argv[2]
   #x = np.array(trainx)
   #y = np.array(trainy)
   #print trainx[0]
   #print trainy[0]
+  read_facts(argv[2])
   reg = linear_model.Ridge (alpha = .5)
 
 
