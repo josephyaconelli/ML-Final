5e37c602131b009fa41cb1910038c42a636beaf3
Liz Olson <lizolson95@gmail.com>
2017-03-11 19:17:13 -0800

    removed bad .csv

--- proj.py
+++ proj.py
@@ -1,4 +1,5 @@
-
+import sys
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
@@ -11,14 +12,22 @@ def read_data(filename):
   data = []
   header = f.readline().strip()
   varnames = p.split(header)
+  print varnames
   namehash = {}
   for l in f:
-    example = [int(x) for x in p.split(l.strip())]
-    x = example[0:-1]
-    y = example[-1]
+    example = [x for x in p.split(l.strip())]
+    #print x[0]
+    #print x[1]
+    #print x[2]
+    #print x[3]
+    #x = example[0:-1]
+    #y = example[-1]
     # Each example is a tuple containing both x (vector) and y (int)
-    data.append( (x,y) )
-  return (data, varnames)
+    xdata = []
+    ydata = []
+    #xdata.append(x)
+    #ydata.append(y)
+  return (xdata, ydata, varnames)
   
   # Load train and test data.  Learn model.  Report accuracy.
 def main(argv):
@@ -27,17 +36,17 @@ def main(argv):
   if (len(argv) != 3):
     print 'Usage: perceptron.py <train> <test> <model>'
     sys.exit(2)
-  (train, varnames) = read_data(argv[0])
-  (test, testvarnames) = read_data(argv[1])
-  modelfile = argv[2]
-  ytemp = []
-  for instance in train:
-    ytemp.append(instance.pop(len(varnames)))
-  x = np.array(train)
-  y = np.array(ytemp)
-  print train[2]
-  print y[0]
-  print y[1]
-  print y[2]
-  print y[3]
+  (trainx, trainy, varnames) = read_data(argv[0])
+  (testx, testy, testvarnames) = read_data(argv[1])
+  #modelfile = argv[2]
+  #x = np.array(trainx)
+  #y = np.array(trainy)
+  #print trainx[0]
+  #print trainy[0]
   reg = linear_model.Ridge (alpha = .5)
+
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
+
