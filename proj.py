1791442acee7015767b633e3b3abfa782182f540
Liz Olson <lizolson95@gmail.com>
2017-03-12 00:32:14 -0800

    done typing parsing code

--- proj.py
+++ proj.py
@@ -4,8 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
-paramdict = {}
-factdict = {}
+paramdict = {} #dictionary so we can print actual parameter titles, not abbreviations
+factdict = {} #keeps track of parameters associated with a county ID number
+trainX = [] #list of X vectors of the training data
+trainY = [] #list of Y output of the training data
+testX = [] #list of X vectors of the testing data
+testY = [] #list of Y output of the testing data
 
 
 def read_facts(filename):
@@ -13,14 +17,11 @@ def read_facts(filename):
   p = re.compile(',')
   f.readline()
   f.readline()
-  count = 0
   for l in f:
     value = p.split(l.strip())
     key = value.pop(0)
     #print key
-    #print len(value[0])
-    if len(value[1]) < 1:
-      count += 1
+    if len(value[1]) < 1: #gets rid of rows for just states
       continue
     value.pop(0)
     value.pop(0)
@@ -34,49 +35,40 @@ def read_param(filename):
     list = p.split(l.strip())
     paramdict[list[0]] = list[1]
 
-def read_data(filename):
+def train_results(filename):
   f = open(filename, 'r')
   p = re.compile(',')
-  data = []
-  header = f.readline().strip()
-  varnames = p.split(header)
-  print varnames
-  namehash = {}
   for l in f:
-    example = [x for x in p.split(l.strip())]
-    #print x[0]
-    #print x[1]
-    #print x[2]
-    #print x[3]
-    #x = example[0:-1]
-    #y = example[-1]
-    # Each example is a tuple containing both x (vector) and y (int)
-    xdata = []
-    ydata = []
-    #xdata.append(x)
-    #ydata.append(y)
-  return (xdata, ydata, varnames)
-  
-  # Load train and test data.  Learn model.  Report accuracy.
+    #print l
+    example = p.split(l.strip())
+    #print example[2]  #float form
+    trainX.append(factdict.get(example[2][:-2]))
+    trainY.append(example[4])
+
+def test_results(filename):
+  f = open(filename, 'r')
+  p = re.compile(',')
+  for l in f:
+    #print l
+    example = p.split(l.strip())
+    #print example[2][:-2]
+    testX.append(example[4])
+    testY.append(factdict.get(example[2][:-2]))
+
 def main(argv):
-  # Process command line arguments.
-  # (You shouldn't need to change this.)
   if (len(argv) != 5):
     print 'Usage: perceptron.py <train> <test> <facts> <dictionary> <model>'
     sys.exit(2)
   read_param(argv[3])
-  #(trainx, trainy, varnames) = read_data(argv[0])
-  #(testx, testy, testvarnames) = read_data(argv[1])
-  #modelfile = argv[2]
-  #x = np.array(trainx)
-  #y = np.array(trainy)
-  #print trainx[0]
-  #print trainy[0]
   read_facts(argv[2])
+  train_results(argv[0])
+  test_results(argv[0])
+  #read_results(argv[1], testX, testY)
+  #print trainX[0]
+  #print trainY[0]
   reg = linear_model.Ridge (alpha = .5)
 
 
 
 if __name__ == "__main__":
   main(sys.argv[1:])
-
